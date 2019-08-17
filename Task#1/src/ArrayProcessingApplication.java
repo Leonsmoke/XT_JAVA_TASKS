6ccdd0d22b6db56744bf43c1ffdcb9e2a94be26e
Leonsmoke <mclav.css@yandex.ru>
2019-08-17 15:08:02 +0400

    1st Practice, 1,2 Task done!

---
+++ Task#1/src/ArrayProcessingApplication.java
@@ -0,0 +1,204 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Melnikov Dmitrii
+ * Practice#1
+ * Task#1
+ */
+public class ArrayProcessingApplication {
+
+    private static final int ARRAY_SIZE = 20;         // Размер массива
+    private static final int MAX_RANDOM_INT = 10;     // Верхняя граница случайного числа
+    private static final int MIN_RANDOM_INT = -10;    // Нижняя граница случайного числа
+
+    private int[] integerArray = new int[ARRAY_SIZE];
+
+    public static void main(String[] args) {
+        ArrayProcessingApplication application = new ArrayProcessingApplication();
+        application.run();
+    }
+
+    public void run(){
+        fillArrayWithRandomIntegers();
+        printAllItemFromArray();
+        while (true){
+            switch (getTaskNumber()){
+                case 0: System.exit(0);
+                case 1:
+                    swapMaxNegativeNumWithMinPositiveNum();
+                    printAllItemFromArray();
+                    break;
+                case 2:
+                    printSumOfEvenItems();
+                    break;
+                case 3:
+                    replaceNegativeNumWithZero();
+                    printAllItemFromArray();
+                    break;
+                case 4:
+                    tripleAllPositiveNumsBeforeNegative();
+                    printAllItemFromArray();
+                    break;
+                case 5:
+                    printDifferenceBetweenAverageAndMinNum();
+                    break;
+                case 6:
+                    printRepeatedOddNum();
+                    break;
+            }
+        }
+    }
+
+    private int getTaskNumber(){
+        Scanner scanner = new Scanner(System.in);    // Создаем сканнер, для ввода номера задания
+        int task = 0;
+
+        do {
+            System.out.println("\nSelect task number (1-6) '0' to exit: ");
+            try{
+                task=scanner.nextInt();
+            }catch (InputMismatchException e){
+                System.err.println("Please enter a number.");
+                task=-1;
+            }
+        } while (task < 0 && task > 6);
+        return task;
+    }
+
+    private void fillArrayWithRandomIntegers(){
+        int randomRange = MAX_RANDOM_INT + Math.abs(MIN_RANDOM_INT) + 1;     // Интервал случайного числа
+
+        for (int index = 0; index < ARRAY_SIZE;index++){
+            integerArray[index] = (int)(Math.random()*randomRange) - MAX_RANDOM_INT;
+        }
+        System.out.println("Array has been filed!");
+    }
+
+    private void printAllItemFromArray(){
+
+        // Выводим в консоль каждый элемент массива
+        for (int singleInt: integerArray
+        ) {
+            System.out.print(singleInt+" ");
+        }
+        System.out.print("\n");
+    }
+
+    private void swapMaxNegativeNumWithMinPositiveNum (){
+        int maxNegativeNumberPosition = -1;                 // Максимальное отрицательное число
+        int minPositiveNumberPosition = -1;                 // Минимальное положительное число
+
+        for (int index = 0; index < ARRAY_SIZE; index++){
+            if (integerArray[index]<0){
+                if (maxNegativeNumberPosition==-1){
+                    maxNegativeNumberPosition=index;
+                } else{
+                    if (integerArray[index]>integerArray[maxNegativeNumberPosition]){
+                        maxNegativeNumberPosition=index;
+                    }
+                }
+            }
+            if (integerArray[index]>0){
+                if (minPositiveNumberPosition==-1){
+                    minPositiveNumberPosition=index;
+                } else{
+                    if (integerArray[index]<integerArray[minPositiveNumberPosition]){
+                        minPositiveNumberPosition=index;
+                    }
+                }
+            }
+        }
+
+        swapNumbers(maxNegativeNumberPosition,minPositiveNumberPosition);
+    }       // 1 Подпункт 1 задания
+
+    private void printSumOfEvenItems(){
+        int sum=0;
+
+        /* т.к. нам нужны четные позиции,
+         * значит нужно брать каждую вторую позицию,
+         * начиная со второго элемента массива, т.е. index = 1
+         */
+        for(int index = 1;index < ARRAY_SIZE; index+=2){
+            sum+=integerArray[index];
+        }
+        System.out.println(sum);
+    }                         // 2 Подпункт 1 задания
+
+    private void replaceNegativeNumWithZero(){
+        for (int index=0; index < ARRAY_SIZE; index++){
+            integerArray[index] = integerArray[index] < 0 ? 0:integerArray[index];
+        }
+    }                  // 3 Подпункт 1 задания
+
+    private void tripleAllPositiveNumsBeforeNegative(){
+        for (int index=0;index<ARRAY_SIZE-1;index++){
+            if (integerArray[index] > 0 && integerArray[index+1] < 0){
+                integerArray[index]*=3;
+            }
+        }
+    }         // 4 Подпункт 1 задания
+
+    private void printDifferenceBetweenAverageAndMinNum(){
+        int sum = 0;                       // Сумма чисел
+        int minNumber = MAX_RANDOM_INT;    // Минимальное число в массиве
+
+        for (int singleInt:integerArray
+             ) {
+            sum+=singleInt;
+            minNumber = singleInt<minNumber ? singleInt : minNumber;
+        }
+        System.out.println(((float)sum/ARRAY_SIZE)-minNumber);
+    }      // 5 Подпункт 1 задания
+
+    private void printRepeatedOddNum(){
+
+        /*
+         * Для поиска одинаковых элементов с нечетными индексами используем вложенный цикл
+         * Начнем с нечетного индекса, т.е. со второй позиции массива first_index=1
+         */
+        for (int first_index=1; first_index<ARRAY_SIZE-1;first_index+=2){
+            for (int second_index=0; second_index<ARRAY_SIZE;second_index++){
+
+                /*
+                 * В цикле перебираются числа с нечетными индексами и сравниваются со всеми остальными
+                 * Если числа не совпадают, то начинается следующая итерация цикла
+                 */
+                if (integerArray[first_index]!=integerArray[second_index] || first_index==second_index){
+                    continue;
+                }
+
+                /*
+                 * Если перебираемые числа совпадают, то в консоли печтается это число.
+                 * А управление передается во внешний цикл.
+                 */
+                System.out.print(integerArray[first_index]+" ");
+                break;
+            }
+        }
+    }                         // 6 Подпункт 1 задания
+
+    private void swapNumbers(int firstNumberPosition, int secondNumberPosition){
+        try{
+
+            //Проверяем, нашлись ли положительные и отрицательные числа в массиве
+            if (firstNumberPosition<0) throw new Exception("There is no negative number in the array");
+            if (secondNumberPosition<0) throw new Exception("There is no positive number in the array");
+
+            //Меняем местами два числа.
+            integerArray[firstNumberPosition]+= integerArray[secondNumberPosition];
+            integerArray[secondNumberPosition] = integerArray[firstNumberPosition]-integerArray[secondNumberPosition];
+            integerArray[firstNumberPosition] = integerArray[firstNumberPosition]-integerArray[secondNumberPosition];
+            /*
+             *int temp = integerArray[firstNumberPosition];
+             *integerArray[secondNumberPosition] = integerArray[firstNumberPosition];
+             *integerArray[firstNumberPosition] = temp;
+             */
+
+        } catch (Exception e){
+            System.err.print(e);
+        }
+    }
+
+}
---
+++ Task#1/src/StringProcessingApplication.java
@@ -0,0 +1,223 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Melnikov Dmitrii
+ * Practice#1
+ * Task#2
+ */
+public class StringProcessingApplication {
+
+    private static final int MAX_NUMBER_OF_STRINGS  = 20;     // Максимальное количество строк для обработки
+    private static final int MIN_NUMBER_OF_STRINGS = 1;       // Минимальное количество строк для обработки
+
+    String[] stringArray;
+
+    public static void main(String[] args) {
+        StringProcessingApplication application = new StringProcessingApplication();
+        application.run();
+    }
+
+    public void run(){
+        int numberOfStrings = getNumberOfString();
+        stringArray = new String[numberOfStrings];
+        while (true){
+            switch (getTaskNumber()){
+                case 0:
+                    System.exit(0);
+                case 1:                                           // 1 подпункт 2 задания
+                    setLines(numberOfStrings);
+                    printShortestLine();
+                    printLongestLine();
+                    break;
+                case 2:                                           // 2 подпункт 2 задания
+                    setLines(numberOfStrings);
+                    printLinesLongerThan(getAverageLength());
+                    break;
+                case 3:                                           // 3 подпункт 2 задания
+                    setLines(numberOfStrings);
+                    printLinesShorterThan(getAverageLength());
+                    break;
+                case 4:                                           // 4 подпункт 2 задания
+                    setLines(numberOfStrings);
+                    printWordWithMinUniqChar();
+                    break;
+                case 5:                                           // 5 подпункт 2 задания
+                    setLines(numberOfStrings);
+                    printWordWithOnlyUniqChar();
+                    break;
+                case 6:
+
+                    break;
+            }
+        }
+    }
+
+    private void getAllLines(){
+        for (String line: stringArray
+             ) {
+            System.out.println(line);
+        }
+    }
+
+    private int getTaskNumber(){
+        Scanner scanner = new Scanner(System.in);    // Создаем сканнер, для ввода номера задания
+        int task = 0;
+
+        do {
+            System.out.println("\nSelect task number (1-6) '0' to exit: ");
+            try{
+                task=scanner.nextInt();
+            }catch (InputMismatchException e){
+                System.err.println("Please enter a number.");
+                task=-1;
+            }
+        } while (task < 0 && task > 6);
+        return task;
+    }
+
+    private int getNumberOfString(){
+        Scanner scanner = new Scanner(System.in);    // Создаем сканнер, для ввода номера задания
+        int numberOfStrings = 0;
+
+        do {
+            System.out.println("\nEnter the number of string to process ("+
+                    MIN_NUMBER_OF_STRINGS+"-"+MAX_NUMBER_OF_STRINGS+") ");
+            try{
+                numberOfStrings=scanner.nextInt();
+            }catch (InputMismatchException e){
+                System.err.println("Please enter a number.");
+                numberOfStrings=-1;
+            }
+        } while (numberOfStrings < MIN_NUMBER_OF_STRINGS && numberOfStrings > MAX_NUMBER_OF_STRINGS);
+        return numberOfStrings;
+    }
+
+    private void setLines(int numberOfStrings){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter "+numberOfStrings+" lines.");
+        for (int index=0;index < numberOfStrings;index++){
+            try{
+                stringArray[index]=scanner.nextLine();
+            } catch (Exception e){
+                System.err.println(e);
+            }
+
+        }
+    }
+
+    private void setWords(int numberOfStrings){
+
+    }
+
+    private void printShortestLine(){
+        int minLength = Integer.MAX_VALUE;
+        String shortestLine = "";
+        for (String line: stringArray
+             ) {
+            if (line.length()<minLength){
+                shortestLine=line;
+                minLength = line.length();
+            }
+        }
+        System.out.println("Shortest line: "+ shortestLine + "\n" +
+                "Length = "+minLength);
+    }
+
+    private void printLongestLine(){
+        int maxLength = Integer.MIN_VALUE;
+        String longestLine = "";
+        for (String line: stringArray
+        ) {
+            if (line.length()>maxLength){
+                longestLine=line;
+                maxLength = line.length();
+            }
+        }
+        System.out.println("Longest line: "+ longestLine + "\n" +
+                "Length = "+maxLength);
+    }
+
+    private float getAverageLength(){
+        float sum = 0;
+
+        for (String line: stringArray
+             ) {
+            sum+=line.length();
+        }
+        return (sum/stringArray.length);
+    }
+
+    private void printLinesLongerThan(float minLength){
+        for (String line: stringArray
+             ) {
+            if (line.length()>minLength){
+                System.out.println(line + "\n" +
+                        "Length = " + line.length());
+            }
+        }
+    }
+
+    private void printLinesShorterThan(float maxLength){
+        for (String line: stringArray
+        ) {
+            if (line.length()<maxLength){
+                System.out.println(line + "\n" +
+                        "Length = " + line.length());
+            }
+        }
+    }
+
+    private void printWordWithMinUniqChar(){
+        int minNumUniqChar = Integer.MAX_VALUE;
+        String minUniqString = "";
+
+        for (String line: stringArray
+        ) {
+            char[] word = line.toCharArray();
+            int uniqChar = 1;
+
+            for (int index=1; index < word.length;index++){
+                for (int index_sec=0; index_sec<index;index_sec++){
+                    if (word[index_sec]==word[index]){
+                        uniqChar--;
+                        break;
+                    }
+                }
+                uniqChar++;
+            }
+            if (uniqChar<minNumUniqChar){
+                minNumUniqChar=uniqChar;
+                minUniqString=line;
+            }
+        }
+        System.out.println(minUniqString);
+    }
+
+    private void printWordWithOnlyUniqChar(){
+        String uniqCharString = null;
+
+        for (String line: stringArray
+        ) {
+            char[] word = line.toCharArray();
+            boolean flag = false;
+
+            if (uniqCharString!=null) break;
+            for (int index=1; index < word.length;index++){
+                for (int index_sec=0; index_sec<index;index_sec++){
+                    if (word[index_sec]!=word[index]){
+                        flag=true;
+                        break;
+                    }
+                }
+
+            }
+            if (flag) continue;
+            uniqCharString = line;
+        }
+        System.out.println(uniqCharString);
+    }
+
+
+}
